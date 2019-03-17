183f078cde9bd8046d75be3fbddd6928f65a978c
Peter G.A. Barraud <barraud@adobe.com>
2019-03-17 10:59:19 +0530

    minor readme updates

--- services/rest.api.php
+++ services/rest.api.php
@@ -3,16 +3,13 @@
   $restapp = new RestfulApp();
   $restapp->run();
 
-  function testrest($param) {
-    echo json_encode($param);
-  }
-
   function validateuser($username, $password){
     require_once('objectlayer/appusercollection.php');
     $filter = array('username' => $username, 'password' => $password);
     $users = new appusercollection($filter);
     echo json_encode($users);
   }
+
   function getappuserbyid($id) {
     require_once('objectlayer/appuser.php');
     $user = new appuser($id);
