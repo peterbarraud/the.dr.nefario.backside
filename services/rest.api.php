c26ef2f66b514fa233e1117433d577e3b16988cb
Peter G.A. Barraud <barraud@adobe.com>
2019-03-17 13:01:05 +0530

    minor formatting updates and datainfo json values made generic

--- services/rest.api.php
+++ services/rest.api.php
@@ -4,7 +4,7 @@
   $restapp->run();
 
   function testrest($param){
-    echo "<div style='background-color:#9baedd;color:#723014;font-size:30px;padding:20px;border:5px solid #723014;'>If you called the <code>testrest</code> API and passed " . $param . " as the parameter, then your <b>Rest server</b> is set up good!</div>";
+    echo "<div style='background-color:#9baedd;color:#723014;font-size:30px;padding:20px;border:5px solid #723014;'>If you called the <code>testrest</code> API and passed <code>" . $param . "</code> as the parameter, then your <b>Rest server</b> is set up good!</div>";
   }
   function validateuser($username, $password){
     require_once('objectlayer/appusercollection.php');
