9463c161c9106076db8f2ca399d09acad7eaec0b
Peter G.A. Barraud <barraud@adobe.com>
2019-03-17 11:09:15 +0530

    put in a test rest method

--- services/rest.api.php
+++ services/rest.api.php
@@ -3,6 +3,9 @@
   $restapp = new RestfulApp();
   $restapp->run();
 
+  function testrest($param){
+    echo "<div style='background-color:#9baedd;color:#723014;font-size:30px;padding:20px;border:5px solid #723014;'>If you called the <code>testrest</code> API and passed " . $param . " as the parameter, then your <b>Rest server</b> is set up good!</div>";
+  }
   function validateuser($username, $password){
     require_once('objectlayer/appusercollection.php');
     $filter = array('username' => $username, 'password' => $password);
